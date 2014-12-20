852705c79ca23252851d06bdaf876b8ebebf0d02
yenduong7727 <yenduong7727@gmail.com>
2014-12-20 02:09:52 +0000

    Add validation for users and posts

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,5 +1,5 @@
 class Micropost < ActiveRecord::Base
   belongs_to :user
   #check :col_name, attribute:
-  validates :content, length: { maximum: 140 }
+  validates :content, length: { maximum: 140 }, presence: true
 end
